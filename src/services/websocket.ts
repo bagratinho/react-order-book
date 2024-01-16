8954328a92f9ad8d4c1a1e1cb1b88cf0385f4b4c
Bagrat Bagoyan <bagratinho@gmail.com>
2024-01-16 00:00:01 -0300

    Update websocket.ts

--- src/services/websocket.ts
+++ src/services/websocket.ts
@@ -8,11 +8,9 @@ class WebSocketService {
   }
 
   init() {
-    store.dispatch(setConnectionStatus("disconnected"));
+    store.dispatch(setConnectionStatus("connecting"));
     this.socket.addEventListener('message', (event) => {
       const data = JSON.parse(event.data);
-      // Dispatch Redux action to update the state
-      // console.log(data);
       if (data.chanId) {
         store.dispatch(setChannelId(data.chanId));
       }
@@ -20,23 +18,44 @@ class WebSocketService {
         store.dispatch(updateOrderBook(data));
       }
     });
-    this.socket.addEventListener('open', (event) => {
-      store.dispatch(setConnectionStatus("connected"));
-      console.log("ws-open")
+    this.socket.addEventListener('open', async (event) => {
+      try {
+        await this.waitForOpenConnection()
+        store.dispatch(setConnectionStatus("connected"));
+        console.log("ws-open")
+      } catch (err) { console.error(err) }
     });
     this.socket.addEventListener('close', (event) => {
-      store.dispatch(setConnectionStatus("disconnected"));
       console.log("ws-closed")
     });
+  }
+
+  waitForOpenConnection() {
+    return new Promise((resolve, reject) => {
+      const maxNumberOfAttempts = 100;
+      const intervalTime = 200;
+  
+      let currentAttempt = 0
+      const interval = setInterval(() => {
+        if (currentAttempt > maxNumberOfAttempts - 1) {
+          clearInterval(interval)
+          reject(new Error('Maximum number of attempts exceeded'))
+        } else if (this.socket.readyState === this.socket.OPEN) {
+          clearInterval(interval)
+          resolve(null);
+        }
+        currentAttempt++
+      }, intervalTime)
+    })
   }
 
   send(message: any) {
     this.socket.send(JSON.stringify(message));
   }
 
-  closeConnection() {
+  closeConnection(dispatchDisconnect: boolean) {
     if (this.socket.readyState === WebSocket.OPEN) {
-      store.dispatch(setConnectionStatus("disconnected"));
+      store.dispatch(setConnectionStatus(dispatchDisconnect ? "disconnected" : "connecting"));
       store.dispatch(deleteOrderBook());
       this.socket.close();
     }
