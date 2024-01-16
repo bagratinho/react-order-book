603e8563bd613bf448d6f0c97cc85e1394524fd2
Bagrat Bagoyan <bagratinho@gmail.com>
2024-01-15 23:58:36 -0300

    Update index.tsx

--- src/features/OrderBook/Header/index.tsx
+++ src/features/OrderBook/Header/index.tsx
@@ -1,6 +1,8 @@
 
 import React from 'react';
+import { useSelector } from 'react-redux';
 import styled from 'styled-components';
+import { RootState } from '../../../app/store';
 
 
 interface IHeaderProps {
@@ -12,14 +14,15 @@ interface IHeaderProps {
 }
 
 const Header = (props: IHeaderProps) => {
+  const connectionStatus = useSelector((state: RootState) => state.orderBook.connectionStatus);
   return (
     <StyledRoot>
       <div>Order book <b>{props.pair}</b></div>
       <div className="actions">
-        <button onClick={props.onConnect}>Connect</button>
-        <button onClick={props.onDisonnect}>Disconnect</button>
-        <button onClick={props.onPrecisionLower}>.0</button>
-        <button onClick={props.onPrecisionHigher}>.00</button>
+        <button disabled={connectionStatus !== "disconnected" ? true : false} onClick={props.onConnect}>Connect</button>
+        <button disabled={connectionStatus !== "connected"} onClick={props.onDisonnect}>Disconnect</button>
+        <button disabled={connectionStatus !== "connected"} onClick={props.onPrecisionLower}>.0</button>
+        <button disabled={connectionStatus !== "connected"} onClick={props.onPrecisionHigher}>.00</button>
       </div>
     </StyledRoot>
   );
